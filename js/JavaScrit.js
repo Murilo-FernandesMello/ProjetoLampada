3018ffff9f888ecbcf2f95c5f2c051b3b1bc83e8
Murilo Fernandes Mello <murilofmello07@gmail.com>
2022-04-12 22:00:19 -0300

    First commit

---
+++ js/JavaScrit.js
@@ -0,0 +1,45 @@
+const i = document.getElementById("off")
+
+function MudarCor(valor) {
+    if (valor == 1) {
+        i.src = "./img/blue.png"
+    }
+
+    if (valor == 2) {
+
+        i.src = "./img/cian.png"
+    }
+    if (valor == 3) {
+        i.src = "./img/darkBlue.png"
+    }
+    if (valor == 4) {
+        i.src = "./img/green.png"
+    }
+    if (valor == 5) {
+        i.src = "./img/lightGreen.png"
+    }
+    if (valor == 6) {
+        i.src = "./img/orange.png"
+    }
+    if (valor == 7) {
+        i.src = "./img/pink.png"
+    }
+    if (valor == 8) {
+        i.src = "./img/purple.png"
+    }
+    if (valor == 9) {
+        i.src = "./img/red.png"
+    }
+    if (valor == 10) {
+        i.src = "./img/yellow.png"
+    }
+    if (valor == 11) {
+        i.src = "./img/white.png"
+    }
+    if (valor == 12) {
+        i.src = "./img/off.png"
+    }
+    if (valor == 13) {
+        i.src = "./img/brown.png"
+    }
+}
